7b7cb8bb0d8d999e7837fd286e94d724ea306526
xena <74474948+a-andres1@users.noreply.github.com>
2021-05-24 18:59:43 -0400

    updated search bar text

--- employeedirectory/src/components/Searchbar.js
+++ employeedirectory/src/components/Searchbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Searchbar = (props) => {
     return (
         <div>
-            <input type="text" onChange={props.functionThatHandlesTheChange} placeholder="Search your heart out"></input>
+            <input type="text" onChange={props.functionThatHandlesTheChange} placeholder="Search"></input>
         </div>
     )
 }
